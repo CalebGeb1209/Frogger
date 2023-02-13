782501a4ab9ad5f19e64e9d773b200347a5d6c17
Caleb Gebrecristos <calebgebrecristos@calebs-mbp-2.attlocal.net>
2023-02-12 19:58:05 -0500

    Detection of whitespace-only, null, and empty names

--- app/src/main/java/com/example/game/ConfigScreen.java
+++ app/src/main/java/com/example/game/ConfigScreen.java
@@ -3,6 +3,15 @@ package com.example.game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ConfigScreen extends AppCompatActivity {
 
@@ -10,5 +19,30 @@ public class ConfigScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        EditText nameEditText = findViewById(R.id.editTextTextPersonName);
+        nameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String name = s.toString().trim();
+                if (name.isEmpty() || name.equals("")) {
+                    nameEditText.setError("Name cannot be empty or contain only whitespaces");
+                } else {
+                    nameEditText.setError(null);
+                }
+            }
+        });
+
     }
 }
