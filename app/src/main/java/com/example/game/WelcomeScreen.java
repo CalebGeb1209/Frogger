c85eeb4734434982d40d73740938be3387f44a07
Yaphet Gebrecristos <yg@yaphets-mbp.attlocal.net>
2023-02-13 01:35:48 -0500

    Just the welcome screen
    Got rid of the config screen

--- app/src/main/java/com/example/game/WelcomeScreen.java
+++ app/src/main/java/com/example/game/WelcomeScreen.java
@@ -2,28 +2,13 @@ package com.example.game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class WelcomeScreen extends AppCompatActivity {
-    private Button playButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        playButton = (Button) findViewById(R.id.button1);
-        playButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openConfigScreen();
-            }
-        });
-    }
-    public void openConfigScreen() {
-        Intent intent = new Intent(this, ConfigScreen.class);
-        startActivity(intent);
     }
 }
