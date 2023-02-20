bb18dfa11bad5582e56b6589145e6016b0506ef6
Srihith Reddy <pallamreddysrihith@gmail.com>
2023-02-20 17:11:27 -0500

    Create Shell Files

---
+++ app/src/main/java/com/example/game/RiverTile.java
@@ -0,0 +1,11 @@
+package com.example.game;
+
+public class RiverTile {
+
+    // includes a list of subobjects in array form
+
+    public RiverTile() {
+
+    } // RiverTile
+
+} // RiverTile
---
+++ app/src/main/java/com/example/game/RoadTile.java
@@ -0,0 +1,11 @@
+package com.example.game;
+
+public class RoadTile {
+
+    // includes a list of subobjects in array form
+
+    public RoadTile() {
+
+    } // RoadTile
+
+} // RoadTile
