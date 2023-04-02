0d60e62551cfa1ebd5881781634f202394fab9c5
Harris Barton <hbarton7@gatech.edu>
2023-04-01 21:42:17 -0400

    added more unit tests

--- app/src/main/java/com/example/game/UnitTestingFunctions.java
+++ app/src/main/java/com/example/game/UnitTestingFunctions.java
@@ -1,9 +1,46 @@
 package com.example.game;
-
+import java.util.Random;
 public class UnitTestingFunctions {
 
     public static int increaseScoreByOne(int currentScore) {
         currentScore = currentScore + 1;
+        return currentScore;
+    }
+
+    public static int DecreaseLives(int currentLives, int playerXCoordinate, int playerYCoordinate) {
+
+        /*
+        * This function tests whether our code correctly decreases the amount of lives to 2 if the player is in a water tile at full health
+        *
+         */
+        boolean[][] gridArray = new boolean[15][15];
+
+        // make the 6th and 7th rows true (equivalent to water tiles)
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
+                if (i == 5 || i == 6) {
+                    gridArray[i][j] = true;
+                } else {
+                    gridArray[i][j] = false;
+                }
+
+            }
+        }
+        if (gridArray[playerXCoordinate][playerYCoordinate] == true) {
+            currentLives -= 1;
+        }
+        return currentLives;
+    }
+
+    public static int changeScore(int currentScore, int playerXCoordinate, int playerYCoordinate) {
+        /*
+        * This function resets the score back to zero if we go into a water tile if we have 3 or 2 lives and keeps current score if we have 1 life (game over)
+         */
+        if (DecreaseLives(3, playerXCoordinate, playerYCoordinate) == 2 || DecreaseLives(2, playerXCoordinate, playerYCoordinate) == 1) {
+            currentScore = 0;
+        } else if (DecreaseLives(1, playerXCoordinate, playerYCoordinate) == 0) {
+            return currentScore;
+        }
         return currentScore;
     }
 }
--- app/src/test/java/com/example/game/SprintFourUnitTests.java
+++ app/src/test/java/com/example/game/SprintFourUnitTests.java
@@ -7,8 +7,71 @@ import org.junit.Test;
 public class SprintFourUnitTests {
 
     @Test
-    public void increaseScoreByOne() {
-        int score = 5;
-        assertEquals(6, UnitTestingFunctions.increaseScoreByOne(score));
+    public void decreaseLivesFromThreeLives() {
+        int playerXCoordinate = 5;
+        int playerYCoordinate = 6;
+        assertEquals(2, UnitTestingFunctions.DecreaseLives(3, playerXCoordinate, playerYCoordinate));
+
+        playerXCoordinate = 3;
+        playerYCoordinate = 6;
+        assertEquals(3, UnitTestingFunctions.DecreaseLives(3, playerXCoordinate, playerYCoordinate));
+
+        playerXCoordinate = 6;
+        playerYCoordinate = 6;
+        assertEquals(2, UnitTestingFunctions.DecreaseLives(3, playerXCoordinate, playerYCoordinate));
+
+        playerXCoordinate = 7;
+        playerYCoordinate = 6;
+        assertEquals(3, UnitTestingFunctions.DecreaseLives(3, playerXCoordinate, playerYCoordinate));
     }
+
+    @Test
+    public void decreaseLivesFromTwoLives() {
+        int playerXCoordinate = 5;
+        int playerYCoordinate = 6;
+        assertEquals(1, UnitTestingFunctions.DecreaseLives(2, playerXCoordinate, playerYCoordinate));
+
+        playerXCoordinate = 3;
+        playerYCoordinate = 6;
+        assertEquals(2, UnitTestingFunctions.DecreaseLives(2, playerXCoordinate, playerYCoordinate));
+
+        playerXCoordinate = 6;
+        playerYCoordinate = 6;
+        assertEquals(1, UnitTestingFunctions.DecreaseLives(2, playerXCoordinate, playerYCoordinate));
+
+        playerXCoordinate = 7;
+        playerYCoordinate = 6;
+        assertEquals(2, UnitTestingFunctions.DecreaseLives(2, playerXCoordinate, playerYCoordinate));
+    }
+    @Test
+    public void decreaseLivesFromOneLife() {
+        int playerXCoordinate = 5;
+        int playerYCoordinate = 6;
+        assertEquals(0, UnitTestingFunctions.DecreaseLives(1, playerXCoordinate, playerYCoordinate));
+
+        playerXCoordinate = 3;
+        playerYCoordinate = 6;
+        assertEquals(1, UnitTestingFunctions.DecreaseLives(1, playerXCoordinate, playerYCoordinate));
+
+        playerXCoordinate = 6;
+        playerYCoordinate = 6;
+        assertEquals(0, UnitTestingFunctions.DecreaseLives(1, playerXCoordinate, playerYCoordinate));
+
+        playerXCoordinate = 7;
+        playerYCoordinate = 6;
+        assertEquals(1, UnitTestingFunctions.DecreaseLives(1, playerXCoordinate, playerYCoordinate));
+    }
+
+    @Test
+    public void reportScore() {
+
+        assertEquals(0, UnitTestingFunctions.changeScore(15, 5, 6));
+
+        assertEquals(20, UnitTestingFunctions.changeScore(20, 3, 6));
+
+        assertEquals(0, UnitTestingFunctions.changeScore(30, 6, 6));
+
+        assertEquals(3, UnitTestingFunctions.changeScore(3, 7, 6));
+    }
+
 }
